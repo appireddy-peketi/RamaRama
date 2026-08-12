059b937b0df05d48e9aae6d0dd7da45758ffbac9
Appireddy <appireddypeketi@gmail.com>
2026-08-13 01:33:59 +0530

    new method is create in Data Base Class

--- src/test/java/Test/database.java
+++ src/test/java/Test/database.java
@@ -8,9 +8,13 @@ public class database {
 	public void DataBase1() {
 		System.out.println("test data base");
 	}
+	@Test
 	public void DateBase2() {
 		System.out.println("Test Data base appireddy 2");
 	}
-	
+	@Test
+	public void DateBase3() {
+		System.out.println("Test Data Base 3");
+	}
 
 }
